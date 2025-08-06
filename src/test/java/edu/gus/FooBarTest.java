1fca64c917a8b7899523026ee7dd01a214dd5c77
Gustavo Fernandes <gustavofernandes330@gmail.com>
2025-08-06 10:29:35 -0300

    Tests for SimpleMath class implemented

---
+++ src/test/java/edu/gus/FooBarTest.java
@@ -0,0 +1,12 @@
+package edu.gus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FooBarTest {
+    @Test
+    public void testFooBar() {
+        System.out.println("Hello JUnit!");
+
+    }
+}
---
+++ src/test/java/edu/gus/SimpleMathTest.java
@@ -0,0 +1,99 @@
+package edu.gus;
+
+import edu.gus.SimpleMath;
+import org.junit.jupiter.api.*;
+
+public class SimpleMathTest {
+    static SimpleMath simpleMath;
+
+    @BeforeAll
+    public static void setup() {
+        System.out.println("Setting up SimpleMath");
+        simpleMath = new SimpleMath();
+    }
+
+
+    @Test
+    public void testSum() {
+        //Setting up variables
+        Double firstNumber = 3D;
+        Double secondNumber = 2D;
+        Double expected = 5D;
+
+        //Executing
+        double value = simpleMath.sum(firstNumber, secondNumber);
+
+        //Validating
+        Assertions.assertEquals(expected, value);
+    }
+
+    @Test
+    public void testSubtraction() {
+        //Setting up variables
+        Double firstNumber = 3D;
+        Double secondNumber = 2D;
+        Double expected = 1D;
+
+        //Executing
+        double value = simpleMath.subtraction(firstNumber, secondNumber);
+
+        //Validating
+        Assertions.assertEquals(expected, value);
+    }
+
+
+    @Test
+    public void testMultiplication() {
+        //Setting up variables
+        Double firstNumber = 3D;
+        Double secondNumber = 2D;
+        Double expected = 6D;
+
+        //Executing
+        double value = simpleMath.multiplication(firstNumber, secondNumber);
+
+        //Validating
+        Assertions.assertEquals(expected, value);
+    }
+
+    @Test
+    public void testDivision() {
+        //Setting up variables
+        Double firstNumber = 3D;
+        Double secondNumber = 2D;
+        Double expected = 1.5D;
+
+        //Executing
+        double value = simpleMath.division(firstNumber, secondNumber);
+
+        //Validating
+        Assertions.assertEquals(expected, value);
+    }
+
+    @Test
+    public void testMean() {
+        //Setting up variables
+        Double firstNumber = 3D;
+        Double secondNumber = 2D;
+        Double expected = 2.5D;
+
+        //Executing
+        double value = simpleMath.mean(firstNumber, secondNumber);
+
+        //Validating
+        Assertions.assertEquals(expected, value);
+    }
+
+    @Test
+    public void testSquareRoot() {
+        //Setting up variables
+        Double number = 9D;
+        Double expected = 3D;
+
+        //Executing
+        double value = simpleMath.squareRoot(number);
+
+        //Validating
+        Assertions.assertEquals(expected, value);
+    }
+}
